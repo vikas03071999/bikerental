793259b9c03f6bdd01531e7536d370290182519a
Vikas Belal <belal.vikas.5@gmail.com>
2023-04-09 10:55:04 +0530

    Bike model image property addd

---
+++ src/data/data.js
@@ -0,0 +1,101 @@
+import RoyalEnfield1 from '../assets/images/RoyalEnfield1.png'
+import Bajaj1 from '../assets/images/Bajaj1.png'
+import Kawasaki1 from '../assets/images/Kawasaki1.png'
+import Honda1 from '../assets/images/Honda1.png'
+import Yamaha1 from '../assets/images/Yamaha1.png'
+import KTM1 from '../assets/images/KTM1.png'
+import BikeModelBullet from '../assets/images/BikeModelBullet.png'
+import BikeModelDominar from '../assets/images/BikeModelDominar.png'
+import BikeModelNinja from '../assets/images/BikeModelNinja.png'
+import BikeModelDuke from '../assets/images/BikeModelDuke.png'
+import BikeModelR15 from '../assets/images/BikeModelR15.png'
+import BikeModelCBR from '../assets/images/BikeModelCBR.png'
+
+export const bikesInformation = [
+    {
+        "bikeName":"Royal Enfield",
+        "bikeRent":"$25",
+        "bikeModel": "Bullet 350",
+        "bikeEngine": "350cc",
+        "cylinders": 1,
+        "torque": "25 Nm",
+        "fuel": "22L",
+        "mileage": "25kmpl",
+        "image":RoyalEnfield1,
+        "modelImage": BikeModelBullet
+    },
+    {
+        "bikeName":"Ninja 300",
+        "bikeRent":"$30",
+        "bikeModel": "Kawasaki",
+        "bikeEngine": "300cc",
+        "cylinders": 2,
+        "torque": "30 Nm",
+        "fuel": "24L",
+        "mileage": "20kmpl",
+        "image":Kawasaki1,
+        "modelImage": BikeModelNinja
+    },
+    {
+        "bikeName":"Dominar",
+        "bikeRent":"$35",
+        "bikeModel": "Bajaj",
+        "bikeEngine": "400cc",
+        "cylinders": 2,
+        "torque": "32 Nm",
+        "fuel": "25L",
+        "mileage": "15kmpl",
+        "image":Bajaj1,
+        "modelImage": BikeModelDominar
+    },
+    {
+        "bikeName":"Duke",
+        "bikeRent":"$22",
+        "bikeModel": "KTM",
+        "bikeEngine": "350cc",
+        "cylinders": 1,
+        "torque": "26 Nm",
+        "fuel": "20L",
+        "mileage": "22kmpl",
+        "image":KTM1,
+        "modelImage": BikeModelDuke
+    },
+    {
+        "bikeName":"Yamaha R15",
+        "bikeRent":"$20",
+        "bikeModel": "Yamaha",
+        "bikeEngine": "150cc",
+        "cylinders": 1,
+        "torque": "22 Nm",
+        "fuel": "24L",
+        "mileage": "30kmpl",
+        "image":Yamaha1,
+        "modelImage": BikeModelR15
+    },{
+        "bikeName":"Honda CBR",
+        "bikeRent":"$30",
+        "bikeModel": "Honda",
+        "bikeEngine": "250cc",
+        "cylinders": 2,
+        "torque": "28 Nm",
+        "fuel": "22L",
+        "mileage": "18kmpl",
+        "image":Honda1,
+        "modelImage": BikeModelCBR
+    }
+]
+
+export const faqs = [
+    {
+        "question": "What is special about comparing rental bike deals?",
+        "answer": "Comparing rental bike deals is important as it helps find the best deal that fits your budget and requirements, ensuring  you get the most value for your money. By comparing various options, you can find deals that offer lower prices, additional services, or  better bike models. You can find bike rental deals by researching online and comparing prices from different rental companies."
+    },
+    {
+        "question": "How to find the bike rental deals?",
+        "answer": "You can find bike rental deals by researching online and comparing prices from different rental companies. Websites such as Expedia, Kayak, and Travelocity allow you to compare prices and view available rental options. It is also recommended to sign up for email newslettersand follow rental bike companies on social media to be informed of any special deals or promotions."
+    },
+    {
+        "question": "How do I find such low bike rental prices?",
+        "answer": "Book in advance: Booking your rental bike ahead of time can often result in lower prices. Compare prices from multiple companies: Use websites like Kayak, Expedia, or Travelocity to compare prices from multiple rental bike companies. Look for discount codes and coupons: Search for discount codes and coupons that you can use to lower the rental price. Renting from an off-airport location can sometimes result in lower prices."
+    }
+]
